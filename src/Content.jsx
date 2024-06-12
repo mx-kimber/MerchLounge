20bef29d23858dab2d6da7d8986bc4bcca92d568
Kimber Ogden <mx.kimber@gmail.com>
2024-06-11 19:21:47 -0500

    remove routes for signup and login from Content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,21 +1,14 @@
 import MerchLounge from "./MerchLounge";
-import { Signup } from "./Signup"
-import { Login } from "./Login"
 import { Routes, Route } from "react-router-dom";
 
 
 export function Content() {
+
   return (
-   
-    <Routes>    
-              
-      <Route path="/MerchLounge" element={<MerchLounge /> } />
-      
-      <Route path="/signup" element={<Signup />} />
-      
-      <Route path="/login" element={<Login />} />
-      
-    </Routes>
-      
-  )
+    <div>
+      <Routes>
+        <Route path="/MerchLounge" element={<MerchLounge />} />
+      </Routes>
+    </div>
+  );
 }
