ef986ae4b69b4dda030bf1efac5b7f58bee21b3b
Kimber Ogden <mx.kimber@gmail.com>
2024-06-17 19:43:17 -0500

    Move indexing logic from Content to ShopsIndex

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,28 +1,15 @@
 import MerchLounge from "./MerchLounge";
 import { AccountSettings } from "./AccountSettings";
 import { Routes, Route } from "react-router-dom";
-import axios from "axios";
-import { useState, useEffect } from "react";
 
 
 export function Content() {
 
-  const [shops, setShops] = useState([]);
-
-  const handleIndexShops = () => {
-    console.log("handleIndexShops");
-    axios.get("http://localhost:3000/shops.json").then((response) => {
-      console.log(response.data);
-      setShops(response.data);
-    });
-  };
-
-  useEffect(handleIndexShops, []);
 
   return (
     <div>
       <Routes>
-        <Route path="/MerchLounge" element={<MerchLounge shops={shops} />} />
+        <Route path="/MerchLounge" element={<MerchLounge />} />
         <Route path="/account_settings" element={<AccountSettings />} />
         
       </Routes>
--- src/ShopsIndex.jsx
+++ src/ShopsIndex.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './shops.css';
+import axios from 'axios';
 
 export function ShopsIndex(props) {
+
+  const [shops, setShops] = useState([]);
+
+  const handleIndexShops = () => {
+    console.log("handleIndexShops");
+    axios.get("http://localhost:3000/shops.json").then((response) => {
+      console.log(response.data);
+      setShops(response.data);
+    });
+  };
+
+  useEffect(handleIndexShops, []);
+
   return (
     <div className='#'>
       <h1>All shops</h1>
 
       <div className='shops-container'>
         
-          {props.shops.map((shop) => (
+          {shops.map((shop) => (
             <div key={shop.id} className='shop-item'>
               <img src={shop.image} alt={shop.shop_name} />
               {/* <div>{shop.shop_name}
@@ -16,10 +31,7 @@ export function ShopsIndex(props) {
               {/* <p>{shop.description}</p> */}
             </div>
           ))}
-
-
       </div>
-
     </div>
   );
 }
