2df2fafe037b23bd7a5e088169deb5337abe62bf
Kimber Ogden <mx.kimber@gmail.com>
2024-06-18 19:02:51 -0500

    Implement handling of account deletion in AccountDelete component

--- src/AccountDelete.jsx
+++ src/AccountDelete.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
+import axios from 'axios';
 
 export function AccountDelete() {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
+  const handleDestroyAccount = () => {
+    console.log("handleDestroyAccount", currentUser);
+    axios.delete(`http://localhost:3000/users/${currentUser.id}.json`)
+      .then((response) => {
+        setCurrentUser(null);
+        window.location.href = "/merchlounge"; 
+      })
+      .catch(error => {
+        console.error("There was an error deleting the account!", error);
+      });
+  };
+
   return (
-    <div>
-      Account Delete Confirmation
+    <div className='container'>
+      <div>
+        This will permanently delete your account. Do you wish to proceed?
+      </div>
+      <button onClick={handleDestroyAccount}>Yes, delete my account.</button> 
     </div>
   )
 }
 
-export default AccountDelete
+export default AccountDelete;
