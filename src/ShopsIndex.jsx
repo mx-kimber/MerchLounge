91dfab7bbe2035789e529cac88154aba6dfcfacc
Kimber Ogden <mx.kimber@gmail.com>
2024-06-20 12:15:34 -0500

    display shop details in a modal  with navigation button in ShopsIndex

--- src/ShopsIndex.jsx
+++ src/ShopsIndex.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './shops.css';
 import axios from 'axios';
+import { Modal } from './Modal';  // Ensure you have a Modal component
+import ShopShow from './ShopShow';  // Ensure you have the ShopShow component
 
 export function ShopsIndex(props) {
-
   const [shops, setShops] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
 
   const handleIndexShops = () => {
     console.log("handleIndexShops");
@@ -17,20 +19,34 @@ export function ShopsIndex(props) {
 
   useEffect(handleIndexShops, []);
 
+  const handleShopShowModal = (shop) => {
+    setModalContent(<ShopShow shop={shop} />);
+    setModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+    setModalContent(null);
+  };
+
   return (
     <div className='container-col'>
       <div className='container-row'>      
         {shops.map((shop) => (
-          <div key={shop.id} className='shop-item'>
+          <div key={shop.id} className='shop-item' onClick={() => handleShopShowModal(shop)} style={{ cursor: 'pointer' }}>
             <img src={shop.image} alt={shop.shop_name} />
-            {/* <div>{shop.shop_name}
-              </div> */}
+            {/* <div>{shop.shop_name}</div> */}
             {/* <p>{shop.description}</p> */}
           </div>
         ))}
       </div>
+      
+      <Modal show={modalVisible} onClose={handleCloseModal}>
+        {modalContent}
+      </Modal>
     </div>
   );
 }
 
 export default ShopsIndex;
+
