e95b5ec8711963302141d5c62893670b95e2551e
Kimber Ogden <mx.kimber@gmail.com>
2024-06-23 20:16:53 -0500

    Update ProductsIndex to display only the User's products

--- src/ProductIndex.jsx
+++ src/ProductIndex.jsx
@@ -1,38 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-
+import { UserContext } from './UserContext'; // Assuming you have a UserContext providing currentUser
 
 export function ProductIndex() {
   const [products, setProducts] = useState([]);
+  const { currentUser } = useContext(UserContext); // Get the current user from context
 
   const handleIndexProducts = () => {
-    console.log("handleIndexProducts");
-    axios.get("http://localhost:3000/products.json").then((response) => {
-      console.log(response.data);
-      setProducts(response.data);
-    });
+    if (currentUser) {
+      console.log("handleIndexProducts");
+      axios.get(`http://localhost:3000/products.json?user_id=${currentUser.id}`).then((response) => {
+        console.log(response.data);
+        setProducts(response.data);
+      }).catch((error) => {
+        console.error('Error fetching products:', error);
+      });
+    }
   };
 
-  useEffect(handleIndexProducts, []);
+  useEffect(handleIndexProducts, [currentUser]);
 
   return (
     <div>
-      <h2>Product Index</h2>
+      {/* <h2>Product Index</h2> */}
       <div className="container-col">
         {products.map((product) => (
           <div key={product.id} className='container-row space-between'>
-            {/* <div>
+            <div>
               <img src={product.product_images} alt={product.product_name} />
-            </div> */}
+            </div>
             <div>
               {product.product_name}
             </div>
             {/* <p>{product.description}</p> */}
             <div>Quantity: {product.quantity}</div>
             <div>{product.price}</div>
-          
             {/* <p>User ID: {product.user_id}</p> */}
-            
             {/* <div className="shops">
               <h3>Shops</h3>
               {product.shops.map((shop) => (
