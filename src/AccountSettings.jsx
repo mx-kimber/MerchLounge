f2024e589858f41fc6e7f0ab7d5920d951e1199b
Kimber Ogden <mx.kimber@gmail.com>
2024-06-24 18:53:27 -0500

    Enhance AccountSettings component with navigation and user context

--- src/AccountSettings.jsx
+++ src/AccountSettings.jsx
@@ -1,35 +1,44 @@
 import React from 'react';
 import UserShow from './UserShow';
 import UsersShopsIndex from './UsersShopsIndex';
-import "./AccountSettings.css"
-import ProductIndex from './ProductIndex';
+import "./AccountSettings.css";
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from './UserContext';
+import { useContext } from 'react';
 
 export function AccountSettings(props) {
+  const { currentUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
+  const handleNavigateToDashboard = () => {
+    navigate('/seller_dashboard'); 
+  };
 
   return (
     <div className="account-settings-container">
       <div className="user-show-container gap-10px">
-        <div>
-          <UserShow />
+        <div className='container-col'>
+          <button onClick={handleNavigateToDashboard}>Seller Dashboard</button> 
+          <button>Shopping Cart</button>
+          <button>Shipping Address</button>
+          <button>Payment Methods</button>
         </div>
-      
-        
       </div>
       
-      <div className="users-shops-index-container"><h2>Seller Dashboard</h2>
+      <div className="users-shops-index-container">
+        <h2>Hello, {currentUser.first_name}!</h2>
         <div className="users-shops-index-container">
-          
-         <ProductIndex />
-        </div><UsersShopsIndex />
-      </div>
-      
+          <div>
+            <UserShow />
+          </div>
+        </div>
+        <div className="users-shops-index-container">
+        <UsersShopsIndex />
+      </div></div>
     </div>
   );
 }
 
 export default AccountSettings;
 
-
-
-// note to self: add button for UserUpdate and CreateShop on modals 
+// Note to self: add button for UserUpdate and CreateShop on modals
