c390fa39263814c10ba28dab9a8bb061680798c4
Kimber Ogden <mx.kimber@gmail.com>
2024-06-29 19:14:59 -0500

    Refactor SellerDashboard component for improved product management

--- src/SellerDashboard.jsx
+++ src/SellerDashboard.jsx
@@ -4,50 +4,44 @@ import ProductIndex from './ProductIndex';
 import SellerDashboardProductShow from './SellerDashboardProductShow';
 
 export function SellerDashboard() {
-  const [selectedProduct, setSelectedProduct] = useState(null);
   const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  const handleProductsLoaded = (loadedProducts) => {
+    setProducts(loadedProducts);
+  };
 
   const handleProductClick = (product) => {
     setSelectedProduct(product);
   };
 
-  const handleProductsLoaded = (loadedProducts) => {
-    setProducts(loadedProducts);
-    if (loadedProducts.length > 0) {
-      setSelectedProduct(loadedProducts[0]);
-    }
-  };
-
   const handleProductUpdate = (updatedProduct) => {
-    const updatedProducts = products.map((product) =>
-      product.id === updatedProduct.id ? updatedProduct : product
+    setProducts(prevProducts => 
+      prevProducts.map(product => 
+        product.id === updatedProduct.id ? updatedProduct : product
+      )
     );
-    setProducts(updatedProducts);
     setSelectedProduct(updatedProduct);
   };
 
   return (
     <div className='container-col'>
-      <h2>Product Index</h2>
-      <div className='container-row grid'>
-        <div>
-          <ProductIndex
-            products={products}
-            onProductClick={handleProductClick}
-            onProductsLoaded={handleProductsLoaded}
-          />
-        </div>
-        <div>
-          <SellerDashboardProductShow
-            product={selectedProduct}
-            onProductUpdate={handleProductUpdate}
-          />
-        </div>
-      </div>
-      <div>
-        <UsersShopsIndex />
-      </div>
-    </div>
+    <div className='container-col padding-10'>
+      <h2>Products</h2><div className='container-row grid'>
+      <ProductIndex 
+        products={products} 
+        onProductClick={handleProductClick} 
+        onProductsLoaded={handleProductsLoaded} 
+      />
+      <SellerDashboardProductShow 
+        product={selectedProduct} 
+        onProductUpdate={handleProductUpdate} 
+      />
+    </div></div>
+    <div>
+    <UsersShopsIndex />
+  </div>
+  </div>
   );
 }
 
